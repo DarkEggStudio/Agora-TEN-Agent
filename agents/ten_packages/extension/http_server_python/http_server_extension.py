f53530803a51b46f2548621bd27098e1b2b07253
darkzero <darkzero_mk2@hotmail.com>
2024-10-28 16:59:21 +0800

    008

--- agents/ten_packages/extension/http_server_python/http_server_extension.py
+++ agents/ten_packages/extension/http_server_python/http_server_extension.py
@@ -68,7 +68,8 @@ class HTTPServerExtension(Extension):
         )
 
         self.server = HTTPServer(
-            (self.listen_addr, self.listen_port), partial(HTTPHandler, ten)
+            ("127.0.0.1", 8080), partial(HTTPHandler, ten)
+            # (self.listen_addr, self.listen_port), partial(HTTPHandler, ten)
         )
         self.thread = threading.Thread(target=self.server.serve_forever)
         self.thread.start()
